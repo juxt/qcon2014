51dd1edef1972a2c7f4919345cf2174274471eec
Malcolm Sparks <malcolm@juxt.pro>
2014-02-27 20:49:54 +0000

    build-all working

--- src-cljs/qcon/main.cljs
+++ src-cljs/qcon/main.cljs
@@ -8,27 +8,30 @@
 
 (enable-console-print!)
 
-(println "Hello QCon!!!!")
+(def app-model
+  (atom {:counters [{:title "Introduction"
+                     :text "This is the introduction"}
+                    {:title "Slide 1"
+                     :text "Here is the first slide"}
+                    {:title "Slide 2"}
+                    {:title "Slide 3"}]}))
 
-(def app-model {:slides
-                [{:title "Introduction"}
-                 {:title "Slide 1"}
-                 {:title "Slide 2"}
-                 {:title "Slide 3"}]})
 
 (defn slide [data owner]
-  (om/component
-   (dom/p nil "SLIDE 1")))
-
-(defn slides [data owner]
-  (om/component
-   (dom/div nil
-            (om/build slide data))))
-
-(defn nav [data owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div nil (dom/p nil "NAV")))))
+      (dom/div nil
+               (dom/h2 nil (:title data))
+               (dom/p nil (:text data))))))
+
+(defn slides [app owner]
+  (reify
+    om/IRenderState
+    (render-state [_ {:keys [message chans]}]
+      (apply dom/div nil
+             (dom/p nil "There are the slides")
+             (om/build-all slide (:counters app)
+                           {:key :id :init-state chans})))))
 
 (om/root slides app-model {:target (.getElementById js/document "content")})
